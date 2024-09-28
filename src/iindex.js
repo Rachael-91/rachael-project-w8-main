e020fcf7493771b19927c00756f933a5b684aa76
Rachael Alexander <rachael@inox-world.com.au>
2024-09-28 18:55:54 +1000

    Updated forecast to use live data from API in JS

--- src/iindex.js
+++ src/iindex.js
@@ -15,6 +15,8 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+
+  getForecast(response.data.city);  
 }
 
 function formatDate(date) {
@@ -50,22 +52,35 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast () {
-  let forecastElement = document.querySelector("#forecast");
+function formatDay (timestamp) {
+let date = new Date(timestamp * 1000);
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+return days[date.getDay()];
+}
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+function getForecast (city) {
+let apiKey = "beeet5337ba0d42b5b6310f3e39o1452";
+let apiURL = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+axios(apiURL).then(displayForecast);
+}
+
+function displayForecast (response) {
   let forecastHtml = "";
 
-  days.forEach (function (day) {
+  response.data.daily.forEach (function (day, index) {
+    if (index > 0 && index < 6) {
     forecastHtml = forecastHtml + `<div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <div><img class="weather-forecast-icon" src="${day.condition.icon_url}"/></div>
     <div class="weather-forecast-temperatures">
-    <div  class="weather-forecast-temperature"><strong>15°</strong></div>
-    <div class="weather-forecast-temperature">9°</div></div>
+    <div  class="weather-forecast-temperature"><strong>${Math.round(day.temperature.maximum)}°</strong></div>
+    <div class="weather-forecast-temperature">${Math.round(day.temperature.minimum)}°</div></div>
     </div>`; 
+
+  }
   });
 
+  let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
  
 }
@@ -74,6 +89,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Sydney");
-
-displayForecast ();
 
